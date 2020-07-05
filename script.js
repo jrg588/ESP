b32996583299d4369fe1f7bb12e5f538619ed6ee
jrg588 <65732415+jrg588@users.noreply.github.com>
2020-07-05 17:54:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 var btnno = document.getElementById('btnno');
 var btnyes = document.getElementById('btnyes');
 
-btnno.addEventListener('mouseenter', () => {
-    console.log("mouse entrando");
-    var pos = coordenadas();
-    btnno.style.top = `${pos.posy}px`;
-    btnno.style.left = `${pos.posx}px`;
-})
+btnno.addEventListener('click', () => {
+    alert('Igual nos casaremos');
+});
 
-var coordenadas = () => {
-    var ancho = screen.width - 100;
-    var alto = screen.height - 200;
-    var posx = Math.random() * (ancho - 0) + 0;
-    var posy = Math.random() * (alto - 0) + 0;
-    return { posx, posy }
-}
 
 btnyes.addEventListener('click', () => {
-    alert('Ya sabia que querias ser mi novio');
+    alert('Ya sabia que querias casarte conmigo');
 });
